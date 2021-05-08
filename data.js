e392c4de7eb673738922b5a7bfcf69c115944f97
Mohmad012 <mohmadailsia@gmail.com>
2021-05-08 08:04:57 +0200

    finish tiktok backend

---
+++ data.js
@@ -0,0 +1,37 @@
+  const date = [
+              {
+                id:1,
+                url:'./Vedios/irto.mp4',
+                poster:"irto.jpg",
+                channel:"ErtugrulGhazi",
+                desc:"This Is Tiktok Clone Build By MERN Stack",
+                song:"the Headline of element",
+                likes:111,
+                shares:222,
+                messages:333
+              },
+              {
+                id:2,
+                url:'./Vedios/osm.mp4',
+                poster:"osm.jpg",
+                channel:"OsmanGazi",
+                desc:"This Is Tiktok Clone Build By MERN Stack",
+                song:"the Headline of element",
+                likes:100,
+                shares:200,
+                messages:300,
+              },
+              {
+                id:3,
+                url:'./Vedios/sult.mp4',
+                poster:"sult.png",
+                channel:"SultanAbdulHamid",
+                desc:"This Is Tiktok Clone Build By MERN Stack",
+                song:"the Headline of element",
+                likes:100,
+                shares:50,
+                messages:90,
+              },
+    ]
+
+export default date
